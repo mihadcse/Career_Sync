cda98640ffb80d5d9aeb4b00f09e9c4b4fe6b698
mihadcse <huzzatullah@iut-dhaka.edu>
2025-03-28 15:14:39 +0600

    Added a schema in a newly created Jobs.js

---
+++ Career_Sync_Server/jobs.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+  companyName: { type: String, required: true },
+  jobTitle: { type: String, required: true },
+  companyLogo: { type: String, required: true },
+  minPrice: { type: Number, required: true },
+  maxPrice: { type: Number, required: true },
+  salaryType: { type: String, enum: ["Yearly", "Monthly"], required: true },
+  jobLocation: { type: String, required: true },
+  postingDate: { type: Date, required: true },
+  experienceLevel: { type: String, required: true },
+  employmentType: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
+export default Job;
