638ef4b8c7c6945eb33fb01c1b801b62ee69bb56
mihadcse <huzzatullah@iut-dhaka.edu>
2025-03-28 15:31:26 +0600

    Added a route for creating new job in the index.js

--- Career_Sync_Server/index.js
+++ Career_Sync_Server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import Job from './jobs.js'
 
 
 const app = express()
@@ -14,14 +15,30 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGODBURL)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.log(err));
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err));
 ;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+app.post('/api/post-job', async (req, res) => {
+    try {
+        const jobData = req.body;
+
+        // Create a new job instance
+        const newJob = new Job(jobData);
+
+        // Save to database
+        await newJob.save();
+
+        res.status(201).json({ message: "Job created successfully", job: newJob });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
