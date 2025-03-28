ffedd01b28fa405dfffe1220a39078211a8453be
mihadcse <huzzatullah@iut-dhaka.edu>
2025-03-28 23:06:20 +0600

    Add company schema in newly created company.js to register new company in Career_sync

---
+++ Career_Sync_Server/company.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const companySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    location: { type: String, required: true },
+    website: { type: String },
+    description: { type: String },
+}, { timestamps: true });
+
+const Company = mongoose.model('Company', companySchema);
+export default Company;
