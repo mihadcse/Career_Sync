9711b98a4ddb770dca02fd7bf1af3f01f78e1cd2
mihadcse <huzzatullah@iut-dhaka.edu>
2025-04-13 16:54:09 +0600

    Merged and Updated files from Zawad's branch. Sidebar Updated with more filtering options

--- Career_Sync_Client/src/sidebar/Button.jsx
+++ Career_Sync_Client/src/sidebar/Button.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({onClickHandler, value, title}) => {
+const Button = ({ onClickHandler, value, title }) => {
   return (
-    <button onClick={onClickHandler} value={value} className={`px-4 py-1 `}>
-        {title}
+    <button
+      onClick={onClickHandler}
+      value={value}
+      className={"py-1 px-4 border text-base hover:bg-blue hover:text-white"}
+    >
+      {title}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- Career_Sync_Client/src/sidebar/Sidebar.jsx
+++ Career_Sync_Client/src/sidebar/Sidebar.jsx
@@ -1,33 +1,8 @@
-// import React from 'react';
-// import Location from './Location';
-
-// const Sidebar = ({ handleChange, handleClick }) => {
-//   return (
-//     <div className='space-y-5 p-4 shadow rounded-lg'>
-//       <h3 className='text-lg font-semibold mb-2 text-center'>Filter</h3>
-
-//       {/* Location filter (Radio buttons, dropdown, etc.) */}
-//       <Location handleChange={handleChange} />
-
-//       {/* Example category filter buttons */}
-//       <div className="space-y-2">
-//         <h4 className='font-semibold'>Job Type</h4>
-//         <button value="Remote" onClick={handleClick} className='w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600'>
-//           Remote
-//         </button>
-//         <button value="Onsite" onClick={handleClick} className='w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600'>
-//           Onsite
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
 
 import React from 'react';
 import Location from './Location';
 import Salary from './Salary';
+import JobPostingData from './JobPostingData';
 
 const Sidebar = ({ handleChange, handleClick }) => {
   return (
@@ -41,6 +16,9 @@ const Sidebar = ({ handleChange, handleClick }) => {
 
     <div className="space-y-5 p-4 shadow-md rounded-xl shadow-cyan-500">
       <Salary handleChange={handleChange} handleClick={handleClick} />
+    </div>
+    <div className="space-y-5 p-4 shadow-md rounded-xl shadow-cyan-500">
+    <JobPostingData handleChange={handleChange} />
     </div>
     </>
   );
