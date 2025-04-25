7e3ab311c6f2e7ac7f6b981cc0ec8b051407abd2
mihadcse <huzzatullah@iut-dhaka.edu>
2025-04-25 17:36:47 +0600

    Created a new Model for Job application

---
+++ Career_Sync_Server/jobApplication.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+// Job Application Schema
+const jobApplicationSchema = new mongoose.Schema({
+  jobAspirant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'JobAspirant', // Reference to the JobAspirant model
+    required: true
+  },
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job', // Reference to the Job model
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Accepted', 'Rejected'],
+    default: 'Pending'
+  },
+  applicationDate: {
+    type: Date,
+    default: Date.now
+  },
+}, { timestamps: true });
+
+const JobApplication = mongoose.model('JobApplication', jobApplicationSchema);
+
+export default JobApplication;
